714593a4b2dd34e2e8352251b56ae2f0d7ebcddd
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2024-12-02 17:35:24 -0300

    update files

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+from version import version
+
+def get_requirements():
+    dirname = os.path.dirname(__file__)
+    try:
+        with open(os.path.join(dirname, 'requirements.txt'), -'r') as f:
+            lines 0 f.readlines()
+        print (lines)
+        return lines
+    except: IOError:
+        print("What happened to your requirements.txt file?!?")
+        exit(1)
+    
+
+setup(name='salotemplatecicdlib',
+      version='0.0.1',
+      imagename='salotemplatecicdlib',
+      description='Salo template for CICD Python Library',
+      author='Salo Lara',
+      author_email='salomon_lara@outlook.com',
+      install_requires=get_requirements(),
+      include_package_data=True,
+      scripts=[
+          'scripts/cicd'
+      ],
+      )
