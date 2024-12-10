cd7f82e9eca9fde34aa0b70fc5524f91a50283f0
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2024-12-09 21:46:12 -0300

    adding stage for python validation

--- setup.py
+++ setup.py
@@ -8,8 +8,18 @@ pipeline {
                 python3 -m venv $PYTHON_VENV
                 source $PYTHON_VENV/bin/activate
                 pip install -r requirements.txt
-                python3 --version
+                python -V
                 '''
+            }
+        }
+        stage('Validate Python Version') {
+            steps {
+                script {
+                    echo "Checking Python version..."
+                    sh '''
+                    python3 --version || echo "Python 3 is not installed"
+                    '''
+                }
             }
         }
     }
