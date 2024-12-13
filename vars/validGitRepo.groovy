6f15eb8d0aaaf05670bd995f4ea60716a7d77287
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2024-12-13 14:12:16 -0300

    moved

--- vars/validGitRepo.groovy
+++ vars/validGitRepo.groovy
@@ -2,16 +2,16 @@
 def call() {
     
     stages {
-    stage('Github Connection Testing') {
-        steps {
-            sh '''
-            whoami
-            sudo -S bash
-            curl https://api.github.com/repos/asgardsalo/git-dev-repo
-            '''
+        stage('Github Connection Testing') {
+            steps {
+                sh '''
+                whoami
+                sudo -S bash
+                curl https://api.github.com/repos/asgardsalo/git-dev-repo
+                '''
+            }
         }
-    }
-}   
+    }   
     println "Shared Library Function: validGitRepo executed!"
 }
 post {
