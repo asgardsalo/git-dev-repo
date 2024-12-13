accd5e4823beaa59fc160c31544990694432fac2
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2024-12-13 20:34:39 -0300

    adding steps and script

--- vars/validPyVer.groovy
+++ vars/validPyVer.groovy
@@ -2,12 +2,16 @@
 //def PYTHON_VENV = "${WORKSPACE}/venv"
     
     stage ('Validate Python Version') {
-            echo "Validating Python environment"
-            sh '''
-            sudo bash
-            echo "python3 -h"
-            '''
-
+            
+            steps {
+                script {
+                    echo "Validating Python environment"
+                    sh '''
+                    sudo bash
+                    echo "python3 -h"
+                    '''
+                }
+            }
     }
     post {
     failure {
