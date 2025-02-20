c088c7d3511a84a53fdc58504387d1e26227baec
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-20 20:33:14 -0300

    adding export path for aws cli

--- vars/AWSConsole.groovy
+++ vars/AWSConsole.groovy
@@ -5,14 +5,17 @@ def call() {
     node(jenkins_agent) {
         
         stage ("testing_aws") {
-            Baseurl="https://awssalo.signin.aws.amazon.com/console"
+           Baseurl="https://awssalo.signin.aws.amazon.com/console"
            // withCredentials('jenkins-aws')
             sh '''
+            export PATH=$PATH:/usr/local/bin
             aws ec2 describe-vpcs --output table
             aws ec2 describe-instances --output table
             aws s3 ls
             aws iam list-users --output table
-            '''
+            ''' 
+
+            println "PATH: " + System.getenv("PATH")
 
         }
     }
