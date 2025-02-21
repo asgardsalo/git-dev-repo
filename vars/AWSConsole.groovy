ca4d3eb9adab89909c2914eae539a43601ad9241
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-20 21:09:17 -0300

    variables testing

--- vars/AWSConsole.groovy
+++ vars/AWSConsole.groovy
@@ -1,22 +1,24 @@
 def call() {
-
+String gcloudpath="export PATH=$PATH:/usr/local/bin"
+String awspath="export PATH="/usr/local/google-cloud-sdk/bin:$PATH""
+/*
+gcloudpath='export PATH=$PATH:/usr/local/bin'
+            awspath='export PATH="/usr/local/google-cloud-sdk/bin:$PATH"'
+            */
     String jenkins_agent = ""
 
     node(jenkins_agent) {
         
         stage ("testing_aws") {
            Baseurl="https://awssalo.signin.aws.amazon.com/console"
-           // withCredentials('jenkins-aws')
             sh '''
-            export PATH=$PATH:/usr/local/bin
+            
             aws ec2 describe-vpcs --output table
             aws ec2 describe-instances --output table
             aws s3 ls
+            aws s3 
             aws iam list-users --output table
-            ''' 
-
-            println "PATH: " + System.getenv("PATH")
-
+            '''
         }
     }
     
