585eef30ba2c6d01014c503b8ab2fb51914ef978
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-21 17:33:29 -0300

    testing path

--- vars/GCPConsole.groovy
+++ vars/GCPConsole.groovy
@@ -1,15 +1,14 @@
 def call() {
-    //export PATH="/usr/local/google-cloud-sdk/bin:$PATH"
+    //
     String jenkins_agent = ""
 
     node(jenkins_agent) {
         
         stage ("testing_gcp") {
            //Baseurl="https://awssalo.signin.aws.amazon.com/console"
-            /*sh '''
-            
-            gcloud config list
-            '''*/
+            sh '''
+            export PATH="/usr/local/google-cloud-sdk/bin:$PATH"
+            '''
             print("$PATH")
         }
     }
