47085f73f60af41596bbe5ab9b93b5c5cee629e2
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-24 16:58:22 -0300

    updated

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -33,7 +33,9 @@
             region= 'sa-east-1'
         break
     }
-    sh 'export PATH=$PATH:/usr/local/bin'
+    sh '''
+        export PATH=$PATH:/usr/local/bin'
+    '''
     sh "aws configure set region ${region}"
     echo $AWS_DEFAULT_REGION
  }
