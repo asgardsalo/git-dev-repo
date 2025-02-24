082229c45db685657aa81bcf6fbb2c2f01b4d56b
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-24 19:20:42 -0300

    updates

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -3,31 +3,43 @@
 def regiSel = ""
  def call (String availabilityzone, String regiSel) {
     case 'N.Virginia':
-        regiSel = 'us-east-1'; break
+        regiSel = 'us-east-1'
+    break
     case 'Ohio':
-        regiSel = 'us-east-2'; break
+        regiSel = 'us-east-2'
+    break
     case 'N.Carolina':
-        regiSel = 'us-west-1'; break
+        regiSel = 'us-west-1'
+    break
     case 'Oregon':
-        regiSel = 'us-west-2'; break
+        regiSel = 'us-west-2'
+    break
     case 'Mumbai':
-        regiSel = 'ap-south-1'; break
+        regiSel = 'ap-south-1'
+    break
     case 'Singapore':
-        regiSel = 'ap-southeast-1'; break
+        regiSel = 'ap-southeast-1'
+    break
     case 'Sydney':
-        regiSel = 'ap-southeast-2'; break
+        regiSel = 'ap-southeast-2'
+    break
     case 'Tokyo':
-        regiSel = 'ap-northeast-1'; break
+        regiSel = 'ap-northeast-1'
+    break
     case 'Frankfurt':
-        regiSel = 'eu-central-1'; break
+        regiSel = 'eu-central-1'
+    break
     case 'London':
-        regiSel = 'eu-west-2'; break
+        regiSel = 'eu-west-2'
+    break
     case 'Stockholm':
-        regiSel = 'eu-north-1'; break
+        regiSel = 'eu-north-1'
+    break
     case 'São Paulo':
-        regiSel = 'sa-east-1'; break
+        regiSel = 'sa-east-1'
+    break
     }
-    
+
     print(regiSel)
 
     stage ("exportpath") {
