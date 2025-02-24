7a0e3e6e78e10f141985f3ab21917e8e4440baaa
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-24 19:00:35 -0300

    updated var

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -2,38 +2,38 @@
  def call (String availabilityzone) {
     switch(availabilityzone) {
         case 'N.Virginia' :
-            region= 'us-east-1'
+            reg-sel= 'us-east-1'
         case 'Ohio' :
-            region= 'us-east-2'
+            reg-sel= 'us-east-2'
         case 'N.Carolina' :
-            region= 'us-west-1'
+            reg-sel= 'us-west-1'
         case 'Oregon' :
-            region= 'us-west-2'
+            reg-sel= 'us-west-2'
         break
 
         case 'Mumbai' :
-            region= 'ap-south-1'
+            reg-sel= 'ap-south-1'
         case 'Singapore' :
-            region= 'ap-southeast-1'
+            reg-sel= 'ap-southeast-1'
         case 'Sydney' :
-            region= 'ap-southeast-2'
+            reg-sel= 'ap-southeast-2'
         case 'Tokyo' :
-            region= 'ap-northeast-1'
+            reg-sel= 'ap-northeast-1'
         break
 
         case 'Frankfurt' :
-            region= 'eu-central-1'
+            reg-sel= 'eu-central-1'
         case 'London' :
-            region= 'eu-west-2'
+            reg-sel= 'eu-west-2'
         case 'Stockholm' :
-            region= 'eu-north-1'
+            reg-sel= 'eu-north-1'
         break
     
         case 'São Paulo' :
-            region= 'sa-east-1'
+            reg-sel= 'sa-east-1'
         break
     }
-    print(region)
+    print(reg-sel)
 
     stage ("exportpath") {
         Baseurl="https://awssalo.signin.aws.amazon.com/console"
@@ -44,7 +44,7 @@
     stage("aws_configure") {
         echo PATH
         //sh "aws configure set region ${region}"
-        echo region
+        echo reg-sel
         }
     }
     
