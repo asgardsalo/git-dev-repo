3cbe147136cf4aa24bf013cf5c71f43e0cbb6a17
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-24 15:54:33 -0300

    moved switch

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -1,9 +1,43 @@
 import utils.Awscomms
  def call(String region)
  
+        switch(availabilityzone) {
+            case 'N. Virginia' :
+                region= 'us-east-1'
+            case 'Ohio' :
+                region= 'us-east-2'
+            case 'N. Carolina' :
+                region= 'us-west-1'
+            case 'Oregon' :
+                region= 'us-west-2'
+            break
 
-void setRegion(String region) {
-    String cmd = "aws configure set region ${region}" 
-    String response = sh(script:cmd, returnStdout:true)
-    print(response)
-}
+            case 'Mumbai' :
+                region= 'ap-south-1'
+            case 'Singapore' :
+                region= 'ap-southeast-1'
+            case 'Sydney' :
+                region= 'ap-southeast-2'
+            case 'Tokyo' :
+                region= 'ap-northeast-1'
+            break
+
+            case 'Frankfurt' :
+                region= 'eu-central-1'
+            case 'London' :
+                region= 'eu-west-2'
+            case 'Stockholm' :
+                region= 'eu-north-1'
+            break
+    
+            case 'São Paulo' :
+                region= 'sa-east-1'
+            break
+
+        }
+
+        void setRegion(String region) {
+            String cmd = "aws configure set region ${region}" 
+            String response = sh(script:cmd, returnStdout:true)
+            print(response)
+        }
