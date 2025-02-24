d736892a68f6b598eff120df4087155ef42e391f
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-24 15:43:11 -0300

    moved switch to pipeline

--- utils/Awscomms.groovy
+++ utils/Awscomms.groovy
@@ -1,5 +1,4 @@
 package utils
-import groovy.json.JsonOutput
 
 //---------Buckets------//
 
@@ -35,41 +34,6 @@ void downloadFromBucket(String bucket, String filenameID) {
 
 //---AWS Configuration------//
 
-switch(reg-sel) {
-
-    case 'N. Virginia' :
-        region= 'us-east-1'
-    case 'Ohio' :
-        region= 'us-east-2'
-    case 'N. Carolina' :
-        region= 'us-west-1'
-    case 'Oregon' :
-        region= 'us-west-2'
-    break
-
-    case 'Mumbai' :
-        region= 'ap-south-1'
-    case 'Singapore' :
-        region= 'ap-southeast-1'
-    case 'Sydney' :
-        region= 'ap-southeast-2'
-    case 'Tokyo' :
-        region= 'ap-northeast-1'
-    break
-
-    case 'Frankfurt' :
-        region= 'eu-central-1'
-    case 'London' :
-        region= 'eu-west-2'
-    case 'Stockholm' :
-        region= 'eu-north-1'
-    break
-    
-    case 'São Paulo' :
-        region= 'sa-east-1'
-    break
-
-}
 void setRegion(String region) {
     String cmd = "aws configure set region ${region}" 
     String response = sh(script:cmd, returnStdout:true)
