7c02dbf736106d002898e0e6f4fad11412652dcb
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-24 19:16:10 -0300

    modify switch

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -2,39 +2,32 @@
 
 def regiSel = ""
  def call (String availabilityzone, String regiSel) {
-    switch(availabilityzone) {
-        case 'N.Virginia' :
-            regiSel= 'us-east-1'
-        case 'Ohio' :
-            regiSel= 'us-east-2'
-        case 'N.Carolina' :
-            regiSel= 'us-west-1'
-        case 'Oregon' :
-            regiSel= 'us-west-2'
-        break
-
-        case 'Mumbai' :
-            regiSel= 'ap-south-1'
-        case 'Singapore' :
-            regiSel= 'ap-southeast-1'
-        case 'Sydney' :
-            regiSel= 'ap-southeast-2'
-        case 'Tokyo' :
-            regiSel= 'ap-northeast-1'
-        break
-
-        case 'Frankfurt' :
-            regiSel= 'eu-central-1'
-        case 'London' :
-            regiSel= 'eu-west-2'
-        case 'Stockholm' :
-            regiSel= 'eu-north-1'
-        break
-    
-        case 'São Paulo' :
-            regiSel= 'sa-east-1'
-        break
+    case 'N.Virginia':
+        regiSel = 'us-east-1'; break
+    case 'Ohio':
+        regiSel = 'us-east-2'; break
+    case 'N.Carolina':
+        regiSel = 'us-west-1'; break
+    case 'Oregon':
+        regiSel = 'us-west-2'; break
+    case 'Mumbai':
+        regiSel = 'ap-south-1'; break
+    case 'Singapore':
+        regiSel = 'ap-southeast-1'; break
+    case 'Sydney':
+        regiSel = 'ap-southeast-2'; break
+    case 'Tokyo':
+        regiSel = 'ap-northeast-1'; break
+    case 'Frankfurt':
+        regiSel = 'eu-central-1'; break
+    case 'London':
+        regiSel = 'eu-west-2'; break
+    case 'Stockholm':
+        regiSel = 'eu-north-1'; break
+    case 'São Paulo':
+        regiSel = 'sa-east-1'; break
     }
+    
     print(regiSel)
 
     stage ("exportpath") {
