4a39181bf5dae6099ebf0e2d99e6c5ed0c39e54a
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-24 17:00:20 -0300

    adding node

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -33,10 +33,13 @@
             region= 'sa-east-1'
         break
     }
-    sh '''
+    node (jenkins_agent) {
+        sh '''
         export PATH=$PATH:/usr/local/bin'
-    '''
-    sh "aws configure set region ${region}"
-    echo $AWS_DEFAULT_REGION
- }
+        '''
+        sh "aws configure set region ${region}"
+        echo $AWS_DEFAULT_REGION
+        }
+    }
+    
     
