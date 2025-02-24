b63264e382695b75d46c4a41c5e3630715a324b6
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-24 19:25:47 -0300

    moved back

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -3,42 +3,47 @@
 def regiSel = ""
 
  def call (String availabilityzone, String regiSel) {
-    case 'N.Virginia':
-        regiSel = 'us-east-1'
-    break
-    case 'Ohio':
-        regiSel = 'us-east-2'
-    break
-    case 'N.Carolina':
-        regiSel = 'us-west-1'
-    break
-    case 'Oregon':
-        regiSel = 'us-west-2'
-    break
-    case 'Mumbai':
-        regiSel = 'ap-south-1'
-    break
-    case 'Singapore':
-        regiSel = 'ap-southeast-1'
-    break
-    case 'Sydney':
-        regiSel = 'ap-southeast-2'
-    break
-    case 'Tokyo':
-        regiSel = 'ap-northeast-1'
-    break
-    case 'Frankfurt':
-        regiSel = 'eu-central-1'
-    break
-    case 'London':
-        regiSel = 'eu-west-2'
-    break
-    case 'Stockholm':
-        regiSel = 'eu-north-1'
-    break
-    case 'São Paulo':
-        regiSel = 'sa-east-1'
-    break
+    switch(availabilityzone) {
+        case 'N.Virginia':
+            regiSel = 'us-east-1'
+        break
+        case 'Ohio':
+            regiSel = 'us-east-2'
+        break
+        case 'N.Carolina':
+            regiSel = 'us-west-1'
+        break
+        case 'Oregon':
+            regiSel = 'us-west-2'
+        break
+        
+        case 'Mumbai':
+            regiSel = 'ap-south-1'
+        break
+        case 'Singapore':
+            regiSel = 'ap-southeast-1'
+        break
+        case 'Sydney':
+            regiSel = 'ap-southeast-2'
+        break
+        case 'Tokyo':
+            regiSel = 'ap-northeast-1'
+        break
+        
+        case 'Frankfurt':
+            regiSel = 'eu-central-1'
+        break
+        case 'London':
+            regiSel = 'eu-west-2'
+        break
+        case 'Stockholm':
+            regiSel = 'eu-north-1'
+        break
+        
+        case 'São Paulo':
+            regiSel = 'sa-east-1'
+        break
+        }
     }
 
     print(regiSel)
@@ -54,6 +59,5 @@ def regiSel = ""
         echo "Region selected: ${regiSel}"
         //sh "aws configure set region ${region}"
         }
-    }
     
     
