93dd503f3842b90d2b326e4897b08cc092640306
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-26 14:05:52 -0300

    testing copilot

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -1,63 +1,96 @@
-//import utils.Awscomms
-
-def regiSel = ""
-
- def call (String availabilityzone, String regiSel) {
-    switch(availabilityzone) {
-        case 'N.Virginia':
-            regiSel = 'us-east-1'
-        break
-        case 'Ohio':
-            regiSel = 'us-east-2'
-        break
-        case 'N.Carolina':
-            regiSel = 'us-west-1'
-        break
-        case 'Oregon':
-            regiSel = 'us-west-2'
-        break
+pipeline {
+    agent any
+    
+    parameters {
+        string(name: 'availabilityzone', defaultValue: '', description: 'Enter the availability zone')
+    }
+    
+    stages {
+        stage('Parameters validation') {
+            steps {
+                script {
+                    if (!params.availabilityzone) {
+                        error("Invalid availability zone")
+                    } else {
+                        echo "Availability Zone Selected: ${params.availabilityzone}"
+                    }
+                }
+            }
+        }
         
-        case 'Mumbai':
-            regiSel = 'ap-south-1'
-        break
-        case 'Singapore':
-            regiSel = 'ap-southeast-1'
-        break
-        case 'Sydney':
-            regiSel = 'ap-southeast-2'
-        break
-        case 'Tokyo':
-            regiSel = 'ap-northeast-1'
-        break
+        stage('Select AWS Region') {
+            steps {
+                script {
+                    switch(params.availabilityzone) {
+                        case 'N.Virginia':
+                            regiSel = 'us-east-1'
+                            break
+                        case 'Ohio':
+                            regiSel = 'us-east-2'
+                            break
+                        case 'N.Carolina':
+                            regiSel = 'us-west-1'
+                            break
+                        case 'Oregon':
+                            regiSel = 'us-west-2'
+                            break
+                        case 'Mumbai':
+                            regiSel = 'ap-south-1'
+                            break
+                        case 'Singapore':
+                            regiSel = 'ap-southeast-1'
+                            break
+                        case 'Sydney':
+                            regiSel = 'ap-southeast-2'
+                            break
+                        case 'Tokyo':
+                            regiSel = 'ap-northeast-1'
+                            break
+                        case 'Frankfurt':
+                            regiSel = 'eu-central-1'
+                            break
+                        case 'London':
+                            regiSel = 'eu-west-2'
+                            break
+                        case 'Stockholm':
+                            regiSel = 'eu-north-1'
+                            break
+                        case 'São Paulo':
+                            regiSel = 'sa-east-1'
+                            break
+                        default:
+                            error("Invalid availability zone")
+                    }
+                    echo "Region selected: ${regiSel}"
+                }
+            }
+        }
         
-        case 'Frankfurt':
-            regiSel = 'eu-central-1'
-        break
-        case 'London':
-            regiSel = 'eu-west-2'
-        break
-        case 'Stockholm':
-            regiSel = 'eu-north-1'
-        break
+        stage('Export Path') {
+            steps {
+                script {
+                    Baseurl = "https://awssalo.signin.aws.amazon.com/console"
+                    sh '''
+                        export PATH=$PATH:/usr/local/bin
+                    '''
+                }
+            }
+        }
         
-        case 'São Paulo':
-            regiSel = 'sa-east-1'
-        break
+        stage('AWS Configure') {
+            steps {
+                script {
+                    echo "PATH: ${env.PATH}"
+                    echo "Region selected: ${regiSel}"
+                    sh "aws configure set region ${regiSel}"
+                }
+            }
         }
     }
-
-    print(regiSel)
-
-    stage ("exportpath") {
-        Baseurl="https://awssalo.signin.aws.amazon.com/console"
-        sh '''
-            export PATH=$PATH:/usr/local/bin
-        '''
-    }
-    stage("aws_configure") {
-        echo "PATH: ${env.PATH}"
-        echo "Region selected: ${regiSel}"
-        //sh "aws configure set region ${region}"
+    
+    post {
+        always {
+            cleanWs()
         }
-    
-    
+    }
+}
