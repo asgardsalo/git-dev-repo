5f4185a623115e62890898efb71de0a0773dc63d
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-27 11:53:11 -0300

    final align

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -3,60 +3,53 @@
 def call (String regiSel) {
 
     stage('Select AWS Region') {
-        //steps {
-            //script {
-                switch (params.region) {
-                    case 'N. Virginia':
-                        regiSel='us-east-1'
-                    break
-                    case 'Ohio':
-                        regiSel='us-east-2'
-                    break
-                    case 'N. Carolina':
-                        regiSel = 'us-west-1'
-                    break
-                    case 'Oregon':
-                        regiSel = 'us-west-2'
-                    break
-                    case 'Mumbai':
-                        regiSel = 'ap-south-1'
-                    break
-                    case 'Singapore':
-                        regiSel = 'ap-southeast-1'
-                    break
-                    case 'Sydney':
-                        regiSel = 'ap-southeast-2'
-                    break
-                    case 'Tokyo':
-                        regiSel = 'ap-northeast-1'
-                    break
-                    case 'Frankfurt':
-                        regiSel = 'eu-central-1'
-                    break
-                    case 'London':
-                        regiSel = 'eu-west-2'
-                    break
-                    case 'Stockholm':
-                        regiSel = 'eu-north-1'
-                    break
-                    case 'São Paulo':
-                        regiSel = 'sa-east-1'
-                    break
-                    default:
-                        echo "Region not recognized: ${params.region}"
-                        error("Invalid region specified.")
-                }
-            //}
-        //}
+ 
+        switch (params.region) {
+            case 'N. Virginia':
+                regiSel='us-east-1'
+            break
+            case 'Ohio':
+                regiSel='us-east-2'
+            break
+            case 'N. Carolina':
+                regiSel = 'us-west-1'
+            break
+            case 'Oregon':
+                regiSel = 'us-west-2'
+            break
+            case 'Mumbai':
+                regiSel = 'ap-south-1'
+            break
+            case 'Singapore':
+                regiSel = 'ap-southeast-1'
+            break
+            case 'Sydney':
+                regiSel = 'ap-southeast-2'
+            break
+            case 'Tokyo':
+                regiSel = 'ap-northeast-1'
+            break
+            case 'Frankfurt':
+                regiSel = 'eu-central-1'
+            break
+            case 'London':
+                regiSel = 'eu-west-2'
+            break
+            case 'Stockholm':
+                regiSel = 'eu-north-1'
+            break
+            case 'São Paulo':
+                regiSel = 'sa-east-1'
+            break
+            default:
+                echo "Region not recognized: ${params.region}"
+                error("Invalid region specified.")
+        }
     }
 
     stage('Export Path') {
-        //steps {
-        //    scirpt {
-                sh "/usr/local/bin/aws configure set region ${regiSel}"
-                sh "/usr/local/bin/aws configure list"
-            }
-        //}
-    //}
+        sh "/usr/local/bin/aws configure set region ${regiSel}"
+        sh "/usr/local/bin/aws configure list"
+    }
 }
 
