a9238f2bdc5913da45795352e6593bea65af7706
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-02-27 11:27:03 -0300

    try other way

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -1,24 +1,22 @@
 #!groovy 
 
-def call () {
+def call (String regiSel) {
+
 String regiSel = ""
- String jenkins_agent = ""
 
-    node(jenkins_agent) {
-
-        stage('Export Path') {
-            steps {
-                script {
-                    sh "/usr/local/bin/aws configure set region ${regiSel}"
-                    sh "/usr/local/bin/aws configure list"
-                }
-            }
-        }
-    
-        post {
-            always {
-            cleanWs()
+    stage('Export Path') {
+        steps {
+            script {
+                sh "/usr/local/bin/aws configure set region ${regiSel}"
+                sh "/usr/local/bin/aws configure list"
             }
         }
     }
+    
+    post {
+        always {
+        cleanWs()
+        }
+    }
 }
+
