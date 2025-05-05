d01b7b3a2f7a9da2b059d79f1e2fdeebae5b6c00
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-05-05 19:18:43 -0400

    adding import line to awsconsole file

--- vars/AWSConsole.groovy
+++ vars/AWSConsole.groovy
@@ -1,3 +1,5 @@
+import resources.templates.services.base-paths
+
 def call() {
 
     String jenkins_agent = ""
@@ -6,8 +8,7 @@ def call() {
         
         stage ("testing_aws") {
            Baseurl="https://awssalo.signin.aws.amazon.com/console"
-            sh '''
-            export PATH=$PATH:/usr/local/bin
+            sh '''xs
             aws ec2 describe-vpcs --output table
             aws ec2 describe-instances --output table
             aws s3 ls
