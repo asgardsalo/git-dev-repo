db76f686ae9397040fd70e612765a0c2b357fffb
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-05-05 20:13:28 -0400

    editing aws and gcp config files

--- vars/AWSConsole.groovy
+++ vars/AWSConsole.groovy
@@ -7,8 +7,7 @@ def call() {
     node(jenkins_agent) {
         
         stage ("testing_aws") {
-           Baseurl="https://awssalo.signin.aws.amazon.com/console"
-            sh '''xs
+            sh '''
             aws ec2 describe-vpcs --output table
             aws ec2 describe-instances --output table
             aws s3 ls
---
+++ vars/GCPConsole.groovy
@@ -0,0 +1,16 @@
+import resources.templates.services.base-paths
+
+def call() {
+
+    String jenkins_agent = ""
+
+    node(jenkins_agent) {
+        
+        stage ("testing_gcp") {
+            sh '''
+            gcloud config list
+            '''
+        }
+    }
+    
+}
