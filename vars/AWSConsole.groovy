f314dfd0be09d1bb9227551eaaa3f6e955b26aa0
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-05-06 19:37:05 -0400

    adding path to awsconsole file

--- vars/AWSConsole.groovy
+++ vars/AWSConsole.groovy
@@ -7,6 +7,8 @@ def call() {
         
         stage ("testing_aws") {
             sh '''
+            export PATH=$PATH:/usr/local/bin
+            echo $PATH
             aws ec2 describe-vpcs --output table
             aws ec2 describe-instances --output table
             aws s3 ls
