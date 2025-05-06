96ba1f4fd073937d81d018c6400061ed2a20997c
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-05-05 21:51:10 -0400

    adding groovy line

--- vars/GCPConsole.groovy
+++ vars/GCPConsole.groovy
@@ -1,17 +1,10 @@
-// Groovy script to interact with GCP Console
+#!groovy
 import resources.templates.services.base-paths
 
 def call() {
-
-    String jenkins_agent = ""
-
-    node(jenkins_agent) {
-        
-        stage ("testing_gcp") {
-            sh '''
-            gcloud config list
-            '''
-        }
+    stage ("testing_gcp") {
+        sh '''
+        gcloud config list
+        '''
     }
-    
 }
