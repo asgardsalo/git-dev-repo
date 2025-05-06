24ff3bcc9044c0f03cd493239708bb5121d0c1b7
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-05-06 18:27:24 -0400

    editing base paths and gcp file

--- vars/GCPConsole.groovy
+++ vars/GCPConsole.groovy
@@ -1,10 +1,8 @@
 #!groovy
-//import resources.templates.services.base-paths
+import resources.templates.services.base-paths
 
 def call() {
     stage ("testing_gcp") {
-        export PATH='$PATH:/usr/local/google-cloud-sdk/bin'
-        echo $PATH
         sh '''
         gcloud config list
         '''
