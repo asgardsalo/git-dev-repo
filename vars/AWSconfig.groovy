a8cdfb4e95dcf8674e65109bc05c98747dc3d371
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-05-06 23:08:01 -0400

    testing option

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -48,11 +48,12 @@ def call (String regiSel) {
     stage('Export Path') {
         sh '''
         export PATH=$PATH:/usr/local/bin
+        export AWS_REGION=${regiSel}
         '''
-        sh "aws configure set region ${regiSel}"
+        /*sh "aws configure set region ${regiSel}"
         sh "aws configure list"
         sh "aws configure set region ${regiSel}"
-        sh "aws configure list"
+        sh "aws configure list"*/
     }
 }
 
