7e28331a6f83fcf07c79786ae0a2e55cc1576e3e
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-05-06 23:20:33 -0400

    testing option 8

--- vars/AWSconfig.groovy
+++ vars/AWSconfig.groovy
@@ -47,8 +47,8 @@ def call (String regiSel) {
 
     stage('Change Region') {
         sh "export PATH=$PATH:/usr/local/bin"
-        sh "aws configure set region ${regiSel}"
-        sh "aws configure list"
+        sh "/usr/local/bin/aws configure set region ${regiSel}"
+        sh "/usr/local/bin/aws configure list"
     }
 }
 
