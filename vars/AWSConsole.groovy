fb7fb1434ba6057983ceef5f7cef54cf221e4f31
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-07-26 18:31:51 -0400

    quitando jenkins agent

--- vars/AWSConsole.groovy
+++ vars/AWSConsole.groovy
@@ -1,19 +1,13 @@
 def call() {
 
-    String jenkins_agent = ""
-
-    node(jenkins_agent) {
-        
-        stage ("testing_aws") {
-            sh '''
-            export PATH=$PATH:/usr/local/bin
-            aws ec2 describe-vpcs --output table
-            aws ec2 describe-instances --output table
-            aws s3 ls
-            aws iam list-users --output table
-            '''
-        }
+  stage ("testing_aws") {
+    sh '''
+        export PATH=$PATH:/usr/local/bin
+        aws ec2 describe-vpcs --output table
+        aws ec2 describe-instances --output table
+        aws s3 ls
+        aws iam list-users --output table
+        '''
     }
-    
-}
+ }
 
