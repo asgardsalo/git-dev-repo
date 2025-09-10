4fc01903947dcc6052936a330f930db748f2a512
asgardsalo <104309730+asgardsalo@users.noreply.github.com>
2025-09-10 09:49:59 -0300

    removing files

--- utils/Gcloud.groovy
+++ utils/Gcloud.groovy
@@ -4,3 +4,13 @@ import groovy.json.JsonOutput
 void setProject(String project) {
     sh(script: "gcloud config set project ${project}", redturnStatus:true, returnStdout: true)
 }
+
+void getProject() {
+    String cmd = 'gcloud config get-value project'
+    print(sh(script:cmd, returnStdout: ))
+}
+
+void describeProject() {
+    String cmd = 'gcloud compute project-info describe'
+    sh(script:cmd)
+}
